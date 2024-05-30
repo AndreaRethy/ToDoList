9d14e0c86e4f78bbd3695652364276d120f9a597
Andrea Rethy <rethyandrea@gmail.com>
2024-05-30 17:53:08 +0200

    testing clearing all the item at once

--- src/to-do-list.test.ts
+++ src/to-do-list.test.ts
@@ -1,5 +1,5 @@
 // Test for adding new items
-import addNewItem  from './to-do-list';
+import { addNewItem, clearList }  from './to-do-list';
 
 
 
@@ -8,4 +8,12 @@ test('Adding new items to existing array', () => {
     addNewItem(itemsArray, 'bread')
 
     expect(itemsArray).toContain('bread');
+});
+
+test('remove all items at once', () => {
+    let itemsArray: string[] = ['buy groceries', 'clean kitchen', 'feed the cat'];
+
+    clearList(itemsArray);
+
+    expect(itemsArray.length).toBe(0);
 });
--- src/to-do-list.ts
+++ src/to-do-list.ts
@@ -1,8 +1,8 @@
 // Declare DOM elements - Comment out before running tests
-const inputField = document.querySelector('#new-item-field') as HTMLInputElement;
-const addButton = document.querySelector('#add-new') as HTMLElement;
-const removeAll = document.querySelector('#clear-all') as HTMLElement;
-const list = document.querySelector('#to-do-list') as HTMLElement;
+// const inputField = document.querySelector('#new-item-field') as HTMLInputElement;
+// const addButton = document.querySelector('#add-new') as HTMLElement;
+// const removeAll = document.querySelector('#clear-all') as HTMLElement;
+// const list = document.querySelector('#to-do-list') as HTMLElement;
 
 // Declare variables
 let itemsArray: string[] = [];
@@ -10,22 +10,22 @@ let itemsArray: string[] = [];
 
 
 // LISTENERS - Comment out before running tests
-addButton.addEventListener('click', () => {
-    const newItem = inputField.value;
-    addNewItem(itemsArray, newItem);
-    list.innerHTML += `<li>${newItem}</li>`
-    const listItems = document.querySelectorAll('#to-do-list li');
-    listItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('done');
-        });
-    });
-});
+// addButton.addEventListener('click', () => {
+//     const newItem = inputField.value;
+//     addNewItem(itemsArray, newItem);
+//     list.innerHTML += `<li>${newItem}</li>`
+//     const listItems = document.querySelectorAll('#to-do-list li');
+//     listItems.forEach(item => {
+//         item.addEventListener('click', () => {
+//             item.classList.toggle('done');
+//         });
+//     });
+// });
 
-removeAll.addEventListener('click', () => {
-    clearList(itemsArray);
-    list.innerHTML = "";
-})
+// removeAll.addEventListener('click', () => {
+//     clearList(itemsArray);
+//     list.innerHTML = "";
+// });
 
 
 
@@ -36,10 +36,10 @@ function addNewItem(array: string[] , newItem: string) {
 }
 
 function clearList(array: string[]) {
-    array = [];
+    array.length = 0;
     console.log(array);
 }
 
 
 
-// export default addNewItem;
+export {addNewItem, clearList};
